e5dcc6ae428e44aa2fbfad962634b75be13565cc
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-08-13 21:12:36 +0900

    Update gif

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ user_session = UserSession()
 data_manage = DataManage(user_session)
 
 UserInputDisplay(user_session, data_manage).show()
+
 if user_session.user_request_ready():
     SearchResultDisplay(user_session, data_manage).show()
     ResultComparison(user_session, data_manage).show()
