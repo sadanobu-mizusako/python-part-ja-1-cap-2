720f7594102ccc8506177014013f30c145d3ed16
sadanobu.mizusako <sadanobu.mizusako@woven.toyota>
2024-08-15 22:56:33 +0900

    Mofify a bit

--- domain_context/db_config.py
+++ domain_context/db_config.py
@@ -1,4 +1,4 @@
 #環境情報
-DB_NAME = "domain_context/car_customize.db"
+DB_NAME = "car_customize.db"
 CREATE_TABLES_SQL_PATH = "domain_context/create_db.sql"
 DB_JSON_DATA_PATH = "domain_context/db_sample.json"
